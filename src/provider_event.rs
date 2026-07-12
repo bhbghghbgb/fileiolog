33c92f4c32cb907eabd38819b00b2bf282b8b6b8
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2026-07-13 02:18:35 +0800

    incomplete attemp 2

--- src/provider_event.rs
+++ src/provider_event.rs
@@ -1,8 +1,10 @@
 #![allow(dead_code)]
 
 use crate::providers::kernel_file::KernelFileEvent;
+use crate::providers::kernel_process::KernelProcessEvent;
 
 #[derive(Debug, Clone)]
 pub enum ProviderEvent {
     KernelFile(KernelFileEvent),
+    KernelProcess(KernelProcessEvent),
 }
