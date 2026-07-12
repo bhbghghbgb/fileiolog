294d940ea136412a90e832a2988fbf1c2b62ceb8
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2026-07-13 02:18:39 +0800

    Revert "incomplete attemp 2"
    This reverts commit 33c92f4c32cb907eabd38819b00b2bf282b8b6b8.

--- src/provider_event.rs
+++ src/provider_event.rs
@@ -1,10 +1,8 @@
 #![allow(dead_code)]
 
 use crate::providers::kernel_file::KernelFileEvent;
-use crate::providers::kernel_process::KernelProcessEvent;
 
 #[derive(Debug, Clone)]
 pub enum ProviderEvent {
     KernelFile(KernelFileEvent),
-    KernelProcess(KernelProcessEvent),
 }
