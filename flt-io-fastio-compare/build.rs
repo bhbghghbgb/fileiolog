888f606591f73c70524a2b919c77f0fe55179931
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2026-08-07 04:09:15 +0800

    Revert "test3 fail"
    This reverts commit ac24bea874c7a6449dbdb421f032f38626d0ad21.

--- flt-io-fastio-compare/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // Embed the manifest requiring administrator privileges for kernel tracing
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_manifest_file("app.manifest");
-        res.compile().expect("Failed to compile Windows resource");
-    }
-}
