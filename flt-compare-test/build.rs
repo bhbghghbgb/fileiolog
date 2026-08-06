a822686191c7b1db54ec8b53a63b6b01feaded94
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2026-08-07 03:46:12 +0800

    Revert "test 2 (fail"
    This reverts commit 417cceff64fef1611a1b9de8684d17b9dfbe960a.

--- flt-compare-test/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_manifest_file("app.manifest");
-        res.compile().expect("Failed to compile Windows resource");
-    }
-}
